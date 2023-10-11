1a42fa27479bd406682eab77b3628efd3cb0da8b
josiasaor <josiasaor@gmail.com>
2023-10-10 21:36:38 -0300

    adjust commit

---
+++ src/components/banner.jsx
@@ -0,0 +1,16 @@
+
+import './banner.css'
+export default function Banner({ personagem }) {
+  return (
+    <div className="banner">
+        <div className='container'>
+
+      <img src={personagem.image} />
+      <h3>Nome: {personagem.name}</h3>
+      <h3>Status: {personagem.status}</h3>
+      <h3>Espécie: {personagem.species}</h3>
+      <h3>Vindo de: {personagem.location.name}</h3>
+        </div>
+    </div>
+  );
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,17 +1,34 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import "./home.css";
 import axios from "axios";
+import Banner from "../components/banner";
+
 export default function Home() {
   const [data, setData] = useState([]);
   const [show, setShow] = useState(false);
   const [procura, setProcuta] = useState("");
+  const [render, setRender] = useState({});
+  const [bannerOn, setBannerON] = useState(false);
   const nome = useParams.nome;
 
   useEffect(() => {
     apiRickAndMorty();
   }, [nome]);
 
+  async function renderizar(id) {
+    try {
+      const response = await axios.get(
+        `https://rickandmortyapi.com/api/character/${id}`
+      );
+      const personagem = response.data;
+      setRender(personagem);
+      console.log(personagem);
+    } catch (error) {
+      console.error("Erro ao buscar os dados:", error);
+    }
+  }
   function apiRickAndMorty() {
     axios
       .get(`https://rickandmortyapi.com/api/character`)
@@ -39,50 +56,58 @@ export default function Home() {
       });
   }
   return (
-    <div>
+    <div className="container">
       <div>
-        <h3>PROCURAR PERSONAGEM</h3>
-        <input
-          onChange={(event) => {
-            setProcuta(event.target.value);
-          }}
-        />
-        <button
-          onClick={() => {
-            procurarPersonagem(procura);
-          }}
-        >
-          GO
-        </button>
-      </div>
+        <div className="search-container">
+          <input
+          placeholder="Procurar personagem"
+            onChange={(event) => {
+              setProcuta(event.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              procurarPersonagem(procura);
+            }}
+          >
+            GO
+          </button>
+        </div>
 
-      {show && (
-        <ul>
-          {data.map((personagem) => (
-            <Link to={`personagem/${personagem.id}`}>
-            
-            <div className="div" key={personagem.id}>
-              <div className="times">
-                <img src={personagem.image} />
-                <div className="nomes">
-                  <p className="p1">{personagem.name}</p>
-                  <p className="p2">{personagem.status}</p>
+        {show && (
+          <ul>
+            {data.map((personagem) => (
+              <div
+                onClick={() => {
+                  renderizar(personagem.id);
+                  setBannerON(true);
+                }}
+              >
+                <div className="div1" key={personagem.id}>
+                  <div className="lista">
+                    <img src={personagem.image} />
+                    <div className="nomes">
+                      <p className="p1">{personagem.name}</p>
+                      <p className="p2">{personagem.status}</p>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-            </Link>
-          ))}
-        </ul>
-      )}
-
-      <button
-        onClick={() => {
-          console.log(data);
-        }}
-      >
-        console.log
-      </button>
-      <h1>HOME PAGE AQUI</h1>
+            ))}
+          </ul>
+        )}
+      </div>
+      <div>
+        {bannerOn && render.name ? (
+          <div>
+            <Banner personagem={render} />
+          </div>
+        ) : (
+          <div>
+            <h1>carregando</h1>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
