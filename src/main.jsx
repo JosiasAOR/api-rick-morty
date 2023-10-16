abfda0915ebadbef7c81453a1cb49449de0ad3e5
josiasaor <josiasaor@gmail.com>
2023-10-16 19:36:18 -0300

    delete react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Home from './pages/home.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './pages/home'
-
-const router = createBrowserRouter([
-  { path: '/', element: <Home /> },
-])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Home />
   </React.StrictMode>,
 )
