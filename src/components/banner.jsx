da6a02471c0765332d71e9f2d809c84d3c416b05
josiasaor <josiasaor@gmail.com>
2023-10-18 20:00:32 -0300

    add google analytics

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -4,7 +4,9 @@ export default function Banner({ personagem }) {
   return (
     <div className="banner">
         <div className='container'>
-
+<button onClick={()=>{
+  console.log(personagem)
+}} >console.log</button>
       <img src={personagem.image} />
       <h3>Nome: {personagem.name}</h3>
       <h3>Status: {personagem.status}</h3>
